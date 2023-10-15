b3705c27546b32cc0ccc61e373ae31687f8ad467
KingYen <1696918097@qq.com>
2023-10-15 22:52:22 +0800

    init

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
 import Link from 'next/link'
 import styles from './page.module.css'
-import Navbar from './components/navbar'
-import Footer from './components/footer'
 
 export default function Home() {
   return (
